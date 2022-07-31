26450f2bef18da337154e1dc60f7d27afe639e63
gongh95 <gonzalo.simon.1995@gmail.com>
2022-07-30 21:43:16 -0300

    creado navbar simple

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import Navbar from "../navbar/navbar";
+
+export default function Header() {
+    return (
+        
+        <>
+            <nav>
+                <p>Crud ReactJs</p>
+                <Navbar/>
+            </nav>
+        </>
+
+    );
+}
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+
+export default function Navbar() {
+    return (
+            
+        <ul>
+            <a href="#">
+                <li>Home</li>
+            </a>
+            <a href="#">
+                <li>About us</li>
+            </a>
+            <a href="#">
+                <li>Contact</li>
+            </a>
+        </ul>
+        
+    );
+}
